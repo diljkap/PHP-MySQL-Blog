669a30abab495b56401b3fb7416c57b63b617b1e
Dilyana <diljkap@gmail.com>
2016-08-27 19:54:06 +0300

    Implementing user registration, login and logout

--- views/users/login.php
+++ views/users/login.php
@@ -2,4 +2,11 @@
 
 <h1><?= htmlspecialchars($this->title) ?></h1>
 
-<!-- TODO: login form will come here ... -->
+<form method="post">
+    <div><label for="username">Username:</label></div>
+    <input id="username" type="text" name="username" />
+    <div><label for="password">Password:</label></div>
+    <input id="password" type="password" name="password" />
+    <div><input type="submit" value="Login"/>
+        <a href="<?=APP_ROOT?>/users/register">[Go to Register]</a></div>
+</form>
